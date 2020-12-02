07fd58b530acc202e06b4a31a14557f0660062b3
Sukumar Hakhoo <sh5661@nyu.edu>
2020-12-02 13:40:14 -0500

    Updating exec.s

--- Data Collection/exec.s
+++ Data Collection/exec.s
@@ -6,11 +6,11 @@
 #SBATCH --gres=gpu:p40:4
 #SBATCH --mem=102400
 #SBATCH --job-name=training_logger
-#SBATCH --time=02:00:00
+#SBATCH --time=04:00:00
 #SBATCH --output=slurm_%j.out
 
 module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2 
 
 source ~/pytorch_env/py3.6.3/bin/activate
 
-# python ~/project2/distributed-training/Data\ Collection/trainer_pytorch.py -c "0" -d data/ -s training_data/
+python ~/project2/distributed-training/Data\ Collection/trainer_pytorch.py -c "12" -d data/ -s training_data/ -w 4
