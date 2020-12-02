e828f73a1a607e23a3caad5f0379bb7d46bb1070
Sukumar Hakhoo <sukumarhakhoo@gmail.com>
2020-12-01 20:41:03 -0500

    Slurm script updates

--- Data Collection/exec.s
+++ Data Collection/exec.s
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --reservation=chung
 #SBATCH --gres=gpu:1
-#SBATCH --gres=gpu:p40:1
+#SBATCH --gres=gpu:p40:4
 #SBATCH --mem=102400
 #SBATCH --job-name=training_logger
 #SBATCH --time=02:00:00
@@ -11,6 +11,7 @@
 
 module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2 
 
-source ../pytorch_env/py3.6.3/bin/activate
+source ~/pytorch_env/py3.6.3/bin/activate
 
-python "project2/distributed-training/Data Collection/trainer_pytorch.py" -c "1,2" -d "data/" -s "training_data/" >> gpu-output.out
+python ~/project2/distributed-training/Data\ Collection/trainer_pytorch.py -e 1 -d data/ -s training_data/
+# python ~/project2/distributed-training/Data\ Collection/trainer_pytorch.py -c "0" -d data/ -s training_data/
