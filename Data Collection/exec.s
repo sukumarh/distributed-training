e3b660750a114c920b81a2196029b36ee3b47706
Sukumar Hakhoo <sukumarhakhoo@gmail.com>
2020-12-06 22:06:28 -0500

    Trainings on Prince

--- Data Collection/exec.s
+++ Data Collection/exec.s
@@ -2,7 +2,6 @@
 #SBATCH --cpus-per-task=28
 #SBATCH --nodes=1
 #SBATCH --reservation=chung
-#SBATCH --gres=gpu:1
 #SBATCH --gres=gpu:p40:4
 #SBATCH --mem=102400
 #SBATCH --job-name=training_logger
@@ -13,4 +12,5 @@ module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2
 
 source ~/pytorch_env/py3.6.3/bin/activate
 
-python ~/project2/distributed-training/Data\ Collection/trainer_pytorch.py -c "15, 16, 17" -d data/ -s training_data/ -w 4
+python distributed_trainer.py -c "32,33,34" -w 16 -d data/ -s training_data/
+
