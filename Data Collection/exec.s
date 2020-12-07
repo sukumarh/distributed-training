f5ce1b77a935224bb1fb4fd8ae81bcadde1849f6
dibyajit30 <dibyajit30@gmail.com>
2020-12-07 12:42:32 -0500

    Config 35 36 outputs

--- Data Collection/exec.s
+++ Data Collection/exec.s
@@ -9,8 +9,8 @@
 #SBATCH --time=04:00:00
 #SBATCH --output=slurm_%j.out
 
-module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2 
+#module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2 
 
-source ~/pytorch_env/py3.6.3/bin/activate
+#source ~/pytorch_env/py3.6.3/bin/activate
 
-python ~/project2/distributed-training/Data\ Collection/trainer_pytorch.py -c "15, 16, 17" -d data/ -s training_data/ -w 4
+python ~/project2/distributed-training/Data\ Collection/trainer_pytorch.py -c "35, 36, 37, 38" -d data/ -s training_data/ -w 4
