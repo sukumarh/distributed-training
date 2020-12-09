855356f3d7087dbc319b62d761224c8c1578407d
Sukumar Hakhoo <sh5661@log-2.nyu.cluster>
2020-12-09 12:12:50 -0500

    Trainings on Greene

--- Data Collection/exec.s
+++ Data Collection/exec.s
@@ -1,18 +1,14 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=28
-#SBATCH --reservation=chung
+#SBATCH --cpus-per-task=20
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:p40:4
+#SBATCH --gres=gpu:v100:4
 #SBATCH --mem=102400
 #SBATCH --job-name=training_logger
-#SBATCH --time=04:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=slurm_%j.out
 
-#module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2 
+module load python/intel/3.8.6 cuda/10.2.89 nccl/cuda10.2/2.7.8  
 
-#source ~/pytorch_env/py3.6.3/bin/activate
-
+source ~/dl/bin/activate
 
 python distributed_trainer.py -c "47,46,45,44" -w 16 -d data/ -s training_data/
-
-
